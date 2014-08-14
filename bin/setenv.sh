89932ab0a8fcd461d57020206670a3c3dd8df855
shuawest@gmail.com <shuawest@gmail.com>
2014-08-13 23:16:23 -0400

    initialing repo

---
+++ bin/setenv.sh
@@ -0,0 +1,4 @@
+
+CATALINA_OPTS="-Xmx2048M -XX:MaxPermSize=512m -Dbtm.root=$CATALINA_HOME -Dbitronix.tm.configuration=$CATALINA_HOME/conf/btm-config.properties -Dorg.jbpm.designer.perspective=RuleFlow"
+
+
