81adefbf547207454eeb0c0b2d91e8dc8ad4d246
shuawest <shuawest@gmail.com>
2014-12-05 14:38:50 -0500

    Changed install script to use /tmp directory for downloads

--- install.sh
+++ install.sh
@@ -33,11 +33,12 @@ if [ ! -f "./bin/catalina.sh" ]; then
 fi 
 
 
-mkdir downloads
-pushd downloads
 
-if [ ! -f jboss-bpms-*-deployable-generic.zip ]; then
+pushd /tmp/
+
+if [ ! -f jboss-bpms-deployable-generic.zip ]; then
 	wget http://people.redhat.com/jowest/jboss-bpms-6.0.3.GA-redhat-1-deployable-generic.zip 
+	mv jboss-bpms-*-deployable-generic.zip jboss-bpms-deployable-generic.zip
 fi
 
 if [ ! -f javax.security.jacc-api-1.5.jar ]; then
@@ -50,40 +51,41 @@ if [ ! -f bpmsontomcat.zip ]; then
 fi
 
 if [ ! -f ojdbc6.jar ]; then
-	wget http://people.redhat.com/jowest/ojdbc6.jar        
+	wget http://people.redhat.com/jowest/ojdbc6.jar     
 fi
 
 if [ ! -f mysql-connector-java.jar ]; then
         wget http://people.redhat.com/jowest/mysql-connector-java.jar
 fi
 
+
 rm -rf bpmsontomcat*/
 unzip bpmsontomcat.zip
 
-
-rm -rf jboss-bpms-*-deployable-generic
-unzip jboss-bpms-*-deployable-generic.zip
-pushd jboss-bpms-*
+rm -rf jboss-bpms-deployable-generic
+unzip jboss-bpms-deployable-generic.zip
+mv jboss-bpms-*-deployable-generic jboss-bpms-deployable-generic
+pushd jboss-bpms-deployable-generic
 unzip jboss-bpms-manager.zip
 unzip jboss-bpms-engine.zip
 popd
 popd
 
-cp -rv downloads/jboss-bpms-*-deployable-generic/jboss-bpms-manager/business-central.war webapps/business-central
-cp -rv downloads/jboss-bpms-*-deployable-generic/jboss-bpms-manager/dashbuilder.war webapps/dashbuilder
+cp -rv /tmp/jboss-bpms-*-deployable-generic/jboss-bpms-manager/business-central.war webapps/business-central
+cp -rv /tmp/jboss-bpms-*-deployable-generic/jboss-bpms-manager/dashbuilder.war webapps/dashbuilder
 
-cp -v downloads/jboss-bpms-*-deployable-generic/jboss-bpms-engine/lib/btm*.jar ./lib/
-cp -v downloads/jboss-bpms-*-deployable-generic/jboss-bpms-engine/lib/jta*.jar ./lib/
-cp -v downloads/jboss-bpms-*-deployable-generic/jboss-bpms-engine/lib/slf4j-api*.jar ./lib/
-cp -v downloads/jboss-bpms-*-deployable-generic/jboss-bpms-engine/lib/slf4j-ext*.jar ./lib/
-cp -v downloads/javax.security.jacc-api-1.5.jar ./lib/
+cp -v /tmp/jboss-bpms-*-deployable-generic/jboss-bpms-engine/lib/btm*.jar ./lib/
+cp -v /tmp/jboss-bpms-*-deployable-generic/jboss-bpms-engine/lib/jta*.jar ./lib/
+cp -v /tmp/jboss-bpms-*-deployable-generic/jboss-bpms-engine/lib/slf4j-api*.jar ./lib/
+cp -v /tmp/jboss-bpms-*-deployable-generic/jboss-bpms-engine/lib/slf4j-ext*.jar ./lib/
+cp -v /tmp/javax.security.jacc-api-1.5.jar ./lib/
 
 cp -v ./webapps/business-central/WEB-INF/lib/kie-tomcat-integration-*.jar ./lib/
 cp -v ./webapps/business-central/WEB-INF/lib/jaxb-api-*.jar ./lib/
 
 # install your database driver
-#cp -v downloads/ojdbc6.jar ./lib/
-cp -v downloads/mysql-connector-java.jar ./lib/
+#cp -v /tmp/ojdbc6.jar ./lib/
+cp -v /tmp/mysql-connector-java.jar ./lib/
 
 mkdir confbackup
 cp --parents conf/tomcat-users.xml confbackup/
@@ -95,7 +97,7 @@ fi
 
 # Overlay file changes from github 
 # NOTE: this will not merge the changes with your files, so check that these overlay files don't disable anything customized in your own Tomcat installation
-cp -rvf downloads/bpmsontomcat*/overlay/* ./
+cp -rvf /tmp/bpmsontomcat*/overlay/* ./
 chmod a+x bin/*.sh
 
 echo " "
